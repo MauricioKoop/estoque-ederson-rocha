c0b0b97a2106fbed2089173b1eedcb2e89375770
Mauricio Eduardo <mauricioeduardokoop@gmail.com>
2026-04-25 20:01:08 -0300

    exibe o estoque ao carregar a página se existir. Crie mensagem de erro para estoque vazio.

--- estoqueEdersonRocha/src/views/js/lista-estoque.js
+++ estoqueEdersonRocha/src/views/js/lista-estoque.js
@@ -1,59 +1,99 @@
 import { API_ESTOQUE } from "./api.js";
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log("DOM pronto! HTML analisado.");
-    const elTabela = document.getElementById("lista-de-estoque");
-    const formBuscaDados = document.getElementById("form-busca-dados");
+document.addEventListener("DOMContentLoaded", (event) => {
+	const elEstoqueVazio = document.getElementById("erroEstoqueVazio");
 
-    formBuscaDados.addEventListener("submit", async (e) => {
-        const btnClicado = e.submitter;
-        e.preventDefault();
+	(async function () {
+		try {
+			let response = await fetch(API_ESTOQUE);
 
-        //captura os valores dos campos codigo material e data
-        const codigoNumericoValue = document.getElementById("codigoNumerico").value;
-        const dataValue = document.getElementById("dataMaterial").value;
+			if (!response.ok) {
+				throw new Error(`Erro na requisição: ${response.status}`);
+			}
 
-        try {
-            // 2. Monta a URL com Query Params (URLSearchParams facilita a limpeza de campos vazios)
-            const params = new URLSearchParams();
-            if(codigoNumericoValue) params.append("codigoNumerico", codigoNumericoValue);
-            if(dataValue) params.append("data", dataValue);
+			const materiais = await response.json();
 
-            //monta a url completa
-            const urlCompleta = `${API_ESTOQUE}?${params.toString()}`;
+			if (materiais.length > 0) {
+				montaTabelaDeEstoque(materiais);
+			}
 
-            let response = await fetch(urlCompleta);
+			//exibe a mensagem de erro se não possuir produtos no estoque
+			exibeMensagemEstoqueVazio(materiais);
+		} catch (error) {
+			console.log("Error: " + error);
+		}
+	})();
 
-            if (!response.ok) {
-                throw new Error(`Erro na requisição: ${response.status}`);
-            }
+	const formBuscaDados = document.getElementById("form-busca-dados");
 
-            const materiais = await response.json();
-            // console.log(materiais);
+	formBuscaDados.addEventListener("submit", async (e) => {
+		const btnClicado = e.submitter;
+		e.preventDefault();
 
-            montaTabelaDeEstoque(materiais);
-        } catch (error) {
-            console.log("Error: " + error);
-        }
-    });
+		//captura os valores dos campos codigo material e data
+		const codigoNumericoValue =
+			document.getElementById("codigoNumerico").value;
+		const dataValue = document.getElementById("dataMaterial").value;
 
-    function montaTabelaDeEstoque(materiais) {
-        elTabela.innerHTML = "<tr><th>Código do Material</th><th>Nome do Material</th><th>Quantidade</th></tr>";
+		try {
+			// 2. Monta a URL com Query Params (URLSearchParams facilita a limpeza de campos vazios)
+			const params = new URLSearchParams();
+			if (codigoNumericoValue)
+				params.append("codigoNumerico", codigoNumericoValue);
+			if (dataValue) params.append("data", dataValue);
 
-        materiais.forEach(material => {
-            const tr = document.createElement("tr");
+			//monta a url completa
+			const urlCompleta = `${API_ESTOQUE}?${params.toString()}`;
 
-            let templateStringDadosTabela = `
+			let response = await fetch(urlCompleta);
+
+			if (!response.ok) {
+				throw new Error(`Erro na requisição: ${response.status}`);
+			}
+
+			const materiais = await response.json();
+			// console.log(materiais);
+
+			//exibe a mensagem de erro se não possuir produtos no estoque
+			exibeMensagemEstoqueVazio(materiais);
+
+			montaTabelaDeEstoque(materiais);
+		} catch (error) {
+			console.log("Error: " + error);
+		}
+	});
+
+	function montaTabelaDeEstoque(materiais) {
+		const elTabela = document.getElementById("lista-de-estoque");
+
+		elTabela.innerHTML =
+			"<tr><th>Código do Material</th><th>Nome do Material</th><th>Quantidade</th><th>Data</th></tr>";
+
+		materiais.forEach((material) => {
+			const tr = document.createElement("tr");
+
+			let templateStringDadosTabela = `
                 <tr>
                     <td>${material.codigoNumerico}</td>
                     <td>${material.nomeMaterial}</td>
                     <td>${material.saldoEstoque}</td>
+					<td>${material.data}</td>
                 </tr>
             `;
 
-            tr.innerHTML = templateStringDadosTabela;
+			tr.innerHTML = templateStringDadosTabela;
 
-            elTabela.appendChild(tr);
-        });
-    }
+			elTabela.appendChild(tr);
+		});
+	}
+
+	function exibeMensagemEstoqueVazio(materiais) {
+		if (materiais.length == 0) {
+			elEstoqueVazio.innerHTML =
+				"Nenhum material cadastrado com essas informações!";
+		}
+
+		//limpa o formulario
+		formBuscaDados.reset();
+	}
 });
